19753e7a7edfdf16c38c26e63d0db20ece70a25c
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-11-05 11:44:48 +0300

    Add a new development branch

---
+++ src/components/SearchBookForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Search() {
+  return (
+    <form id="search-form">
+      <input type="text" placeholder="Search" value="" />
+    </form>
+  );
+}
+export default Search;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Search from "../SearchBookForm";
 
 function Header() {
-  return <h1>Header Section </h1>;
+  return <h1>Header Section
+    <Search/>
+     </h1>;
 }
 
 export default Header;
