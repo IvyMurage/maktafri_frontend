63c11456db9665d2cc7291303be5c932bfb89c80
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-11-06 19:43:01 +0300

    add initial component code and css file

---
+++ src/components/BookPageReview/BookPage.js
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from 'react';
+import './Cards.css';
+
+function BookPage(props){
+    const [book, setBook] = useState ([]);
+
+    useEffect(() => {
+        fetch("http://localhost:9292/books")
+            .then((response) => response.json())
+            .then((data) => setBook(data))
+            .catch(console.log);
+    }, [setBook]);
+
+    return(
+        <>
+            <div className = "book-image-card">
+                {props.image}
+            </div>
+            <div className = "book-description-card">
+                {props.description}
+            </div>
+            <div className = "book-star-rating">
+                {props.star_rating}
+            </div>
+        </>
+    )
+
+}
+
+export default BookPage
