c0f5667c8d63d0d0741ff655a80a81fd9230102d
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-11-07 18:34:01 +0300

    display cards in a grid

--- src/components/BookStyles/BookList.js
+++ src/components/BookStyles/BookList.js
@@ -3,12 +3,20 @@ import { BookContext } from "../BookContext";
 import BookCard from "./BookCard";
 
 function BookList() {
-const {books} = useContext(BookContext)
-    const bookList = books.map(book => {<BookCard key={book.id}/>})
+  const { books } = useContext(BookContext);
+  const bookList = books.map((book) => (
+    <BookCard
+      key={book.id}
+      bookImage={book.image_url}
+      bookTitle={book.title}
+      bookCategory={book.categroy}
+      bookAuthor={book.author.first_name}
+    />
+  ));
   return (
     <div className="book-list">
-        <h3>Popular Now</h3>
-      <BookCard />
+      {/* <h3>Popular Now</h3> */}
+      {bookList}
     </div>
   );
 }
