eac3f676bb59a2a41385da495888a5ded83c10c1
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-11-07 18:32:58 +0300

    Add book details and style image card

--- src/components/BookStyles/BookCard.js
+++ src/components/BookStyles/BookCard.js
@@ -2,12 +2,13 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
-function BookCard() {
+function BookCard({ bookImage, bookTitle, bookAuthor, bookCategory }) {
   return (
     <div className="book-card">
-      <img src={"book.image_url"} alt={"book.title"} />
-      <h4> Book.title </h4>
-      <p>Plant Category</p>
+      <img src={bookImage} alt={bookTitle} />
+      <h4> {bookTitle}</h4>
+      <p>{bookCategory}</p>
+      <p>{bookAuthor}</p>
       <FontAwesomeIcon icon={faStar} />
     </div>
   );
