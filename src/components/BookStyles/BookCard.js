743a368454eb920148f6f3ee6d5fe683426e66cb
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-11-07 22:52:53 +0300

    Add a navigaion hook for the bookpage

--- src/components/BookStyles/BookCard.js
+++ src/components/BookStyles/BookCard.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { BookContext } from "../BookContext";
+import { Link } from "react-router-dom";
 
-function BookCard({ bookImage, bookTitle, bookAuthor, bookCategory }) {
+function BookCard({ bookImage, bookTitle, bookAuthor, bookCategory, book }) {
+  const { handleOnClickBook } = useContext(BookContext);
   return (
     <div className="book-card">
       <img src={bookImage} alt={bookTitle} />
-      <h4> {bookTitle}</h4>
+      <Link to={`/booklist/${book.id}`}>
+        <h4 onClick={() => handleOnClickBook(book)}> {bookTitle}</h4>
+      </Link>
       <p>{bookCategory}</p>
       <p>{bookAuthor}</p>
-      <FontAwesomeIcon icon={faStar} className="fa-star"/>
+      <FontAwesomeIcon icon={faStar} className="fa-star" />
     </div>
   );
 }
