ff4a71aa9119bb7722f34023301749a823aa8108
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-11-07 22:52:16 +0300

    Add routes to navigate to the bookpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import { BookProvider } from "./components/BookContext";
+import BookPage from "./components/BookPageReview/BookPage";
 import BookList from "./components/BookStyles/BookList";
 
 function App() {
   return (
     <div className="App">
       <BookProvider>
-        <BookList />
+        <Routes>
+          <Route
+            path="/booklist"
+            element={
+              <>
+                <h3 id="bookList-heading">Popular Now</h3>
+                <BookList />
+              </>
+            }
+          />
+          <Route path="/booklist/:id" element={<BookPage />} />
+        </Routes>
       </BookProvider>
     </div>
   );
