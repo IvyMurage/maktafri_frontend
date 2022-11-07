36c78a678149d09349862c9671abeef2c8f01de5
Francis-Muriithi <mufraggie@gmail.com>
2022-11-07 14:06:45 +0300

    search section init commit

--- src/components/SearchBookForm.js
+++ src/components/SearchBookForm.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 
 function Search() {
+
+  const [book, setBook] = useState("");
+  const [result, setResult] = useState([]);
+
+  function handleChange(e){
+
+    const book = e.target.value;
+    setBook(book)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    axios.get("http://localhost:9292/")
+    .then(data => {
+      console.log(data.items)
+    })
+
+  }
   return (
-    <form id="search-form">
-      <input type="text" placeholder="Search" value="" />
+    <form onSubmit={handleSubmit}id="search-form">
+      <input type="text" onChange={handleChange} placeholder="Search" value="" />
+      <div>
+        <button type="submit">Search</button>
+      </div>
     </form>
   );
 }
